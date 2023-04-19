33c3307580f7a5a80daf529876b216ef318d62f1
JonPiniesEcheguren <jon.pinies.ech@gmail.com>
2023-04-19 17:56:15 +0200

    Main añadido y cambios en titulos de ejercicios

---
+++ Main.py
@@ -0,0 +1,107 @@
+import os
+import sys
+#from Varios import principal
+
+from EjerciciosConClases import Clases1
+from EjerciciosConClases import Clases2
+from EjerciciosConClases import Clases3
+from EjerciciosConClases import Clases4
+from EjerciciosConJson import Json1
+from EjerciciosConJson import Json2
+from EjerciciosConJson import Json3
+
+while True:
+    if sys.platform.startswith("linux"):
+        # linux
+        os.system('clear') 
+    elif sys.platform == "darwin":
+        # MAX OS X
+        os.system('clear') 
+    elif os.name == "nt":
+        #Windows, Cigwyin, etc. (32/64-bit)
+        os.system('cls') 
+    # os.system("cls") #Limpia la pantalla en la consola
+    # os.system("clear") #Limpia pantalla consola MAC y Linux
+    
+    print("Bienvenidos")
+    print("Menu principal")
+
+    #print("1-Ejercicios con colecciones")
+    #print("2-Ejercicios con compresión de listas")
+    print("3-Ejercicios con JSON")
+    print("4-Ejercicios con clases")
+
+    print("0-Salir")
+
+    opcion = input("Seleccione una opción:")
+
+    #if opcion == "1":
+
+        #print("Menu Ejercicios con colecciones")
+
+        #print("0-Salir")
+
+    #elif opcion == "2":
+
+        #print("Menu Ejercicios con compresión de listas")
+
+        #print("0-Salir")
+
+    if opcion == "3":
+
+        print("Menu Ejercicios con JSON")
+
+        print("1-Primer ejercicio JSON")
+        print("2-Segundo ejercicio JSON")
+        print("3-Tercer ejercicio JSON")
+    
+        print("0-Salir")
+    
+        opcion = input("Seleccione una opción:")
+
+        if opcion == "1":
+            Json1.principal()
+        elif opcion == "2":
+            Json2.principal()
+        elif opcion == "3":
+            Json3.principal() 
+        
+        elif opcion == "0":
+            print("Un placer atenderle. Chao!")
+            break
+            
+        continuar=input("Presione enter para continuar...")
+
+    elif opcion == "4":
+
+        print("Menu Ejercicios con clases")
+
+        print("1-Primer ejercicio clases")
+        print("2-Segundo ejercicio clases")
+        print("3-Tercer ejercicio clases")
+        print("4-Cuarto ejercicio clases")
+    
+        print("0-Salir")
+    
+        opcion = input("Seleccione una opción:")
+    
+        if opcion == "1":
+            Clases1.principal()
+        elif opcion == "2":
+            Clases2.principal()
+        elif opcion == "3":
+            Clases3.principal()
+        elif opcion == "4":
+            Clases4.principal() 
+        
+        elif opcion == "0":
+            print("Un placer atenderle. Chao!")
+            break
+
+        continuar=input("Presione enter para continuar...")
+
+    elif opcion == "0":
+        print("Un placer atenderle. Chao!")
+        break
+
+    continuar=input("Presione enter para continuar...")
